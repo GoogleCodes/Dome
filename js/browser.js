56113f84cf7e3407065195e4a64462180c825b89
‘511810970@qq.com <511810970@qq.com>
2018-01-24 18:06:14 +0800

    vue update

---
+++ js/browser.js
@@ -0,0 +1,8 @@
+/**
+ * Created by SamHong on 2018/1/24.
+ */
+
+$('.ie-nthchild').nthChild({
+    cssClasses: ['odd','even']
+});
+
--- js/tools.js
+++ js/tools.js
@@ -1,133 +1,264 @@
-let ToolsFunction = (function() {
+let tools = (function() {
 
-	class Tools {
-        
-        _$(id) {
-            return document.getElementById(id);
-        }
+    function _$(id) {
+        return document.getElementById(id);
+    }
 
-		swiperBanner() {
-			var mySwiper = new Swiper ('.swiper-container', {
-				loop: true,
-				autoplay: true,
-				pagination: {
-					el: '.swiper-pagination',
-					clickable: true,
-				},
-				navigation: {
-					nextEl: '.swiper-button-next',
-					prevEl: '.swiper-button-prev',
-				},
-			})
-			$('.match_content_left_content').eq(0).show()
-			$('.match_content_left_header_nav_item').click(function(){
-				$('.match_content_left_header_nav_item').removeClass('active')
-				$(this).addClass('active')
-				var index = $('.match_content_left_header_nav_item').index($(this))
-				$('.match_content_left_content').hide()
-				$('.match_content_left_content').eq(index).fadeIn()
-			})
-		}
+    function swiperBanner() {
+        var mySwiper = new Swiper ('.swiper-container', {
+            loop: true,
+            autoplay: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        })
+        $('.match_content_left_content').eq(0).show()
+        $('.match_content_left_header_nav_item').click(function(){
+            $('.match_content_left_header_nav_item').removeClass('active')
+            $(this).addClass('active')
+            var index = $('.match_content_left_header_nav_item').index($(this))
+            $('.match_content_left_content').hide()
+            $('.match_content_left_content').eq(index).fadeIn()
+        })
+    }
 
-		siblings(dom,callback){
-            var pdom = dom.parentElement;
-            var tabArr = [].slice.call(pdom.children);
-            tabArr.filter(function(obj){
-                if(obj!=dom)callback.call(obj);
-            });
-        }
 
-        siblingsDom(id, class1, class2, flag) {
-            var cardDom = this._$(id);
-            var liDomes = cardDom.children;
-            var len = liDomes.length;
-            for(var i = 0; i < len; i++) {
-                var that = this;
-                //给对象缓存自有属性
-                liDomes[i].index = i;
-                liDomes[i].onmouseover = function() {
-                    this.className = class1;
-                    //同辈元素互斥
-                    that.siblings(this,function(){
-                        this.className = class2;
-                    });
-                }
-                liDomes[i].onmouseover = function(){
-                    this.className = class1;
-                    //同辈元素互斥
-                    that.siblings(this, function(){
-                        this.className = class2;
-                    });
-                    //把对应的选项卡的内容显示出来
-                    var tabDom = document.getElementById("IDate_list").children[this.index];
-                    if (flag == true) {
-                        tabDom.style.display = "block";
-                        that.siblings(tabDom, function(){
-                            this.style.display = "none";
-                        });
-                    }
-                };
+    function siblings(dom,callback) {
+        var pdom = dom.parentElement;
+        var tabArr = [].slice.call(pdom.children);
+        tabArr.filter(function(obj){
+            if(obj != dom)callback.call(obj);
+        });
+    }
 
+    function siblingsDom(id, class1, class2, flag) {
+        console.log(_$);
+        var cardDom = document.getElementById(id);
+        var liDomes = cardDom.children;
+        var len = liDomes.length;
+        for(var i = 0; i < len; i++) {
+            var that = this;
+            //给对象缓存自有属性
+            liDomes[i].index = i;
+            liDomes[i].onmouseover = function() {
+                this.className = class1;
+                //同辈元素互斥
+                siblings(this,function(){
+                    this.className = class2;
+                });
             }
-        }
-
-        getNewsMesasge() {
-        	$.ajax({
-        		url: './data/news.json',
-        		method: "GET",
-        		async: true,
-        		success: function(res) {
-        			console.log(res);
-        		},
-        		fail: function(err) {
-        			console.log(err);
-        		}
-        	});
-        }
-
-        mune() {
-            let html = "<div class='column' style='display: none;'>"+
-                        "<ul>" +
-                        "    <li>" +
-                        "        <i class='iconfont icon-185078emailmailstreamline'></i>" +
-                        "        <span>Contact Supplier</span>" +
-                        "    </li>" +
-                        "    <li>" +
-                        "        <i class='iconfont icon-whatsapp'></i>" +
-                        "        <span class='whatsapp'>whatsapp</span>" +
-                        "    </li>" +
-                        "    <li>" +
-                        "        <i class='iconfont icon-wechat'></i>" +
-                        "        <span class='wechat'>wechat</span>" +
-                        "    </li>" +
-                        "    <li class='ruiTouTop'>" +
-                        "        <span class='top'>TOP</span>" +
-                        "        <i class='iconfont icon-top'></i>" +
-                        "    </li>" +
-                        "</ul>" +
-                    "</div>";
-            $('body').append(html);
-            // 按钮淡入淡出
-            $(window).scroll(function(){
-                if($(window).scrollTop() >= 450){
-                    $(".column").fadeIn(200);
-                } else {
-                    $(".column").stop(true,true).fadeOut(200);
+            liDomes[i].onmouseover = function(){
+                this.className = class1;
+                //同辈元素互斥
+                siblings(this, function(){
+                    this.className = class2;
+                });
+                //把对应的选项卡的内容显示出来
+                var tabDom = document.getElementById("IDate_list").children[this.index];
+                if (flag == true) {
+                    tabDom.style.display = "block";
+                    siblings(tabDom, function(){
+                        this.style.display = "none";
+                    });
                 }
-            });
-            
-            // 回到顶部
-            $(".ruiTouTop").click(function(){
-                $("html,body").animate({
-                    scrollTop:0
-                },200);
-            });
-        }
+            };
 
-	}
+        }
+    }
+
+    function getNewsMesasge() {
+        $.ajax({
+            url: './data/news.json',
+            method: "GET",
+            async: true,
+            success: function(res) {
+                console.log(res);
+            },
+            fail: function(err) {
+                console.log(err);
+            }
+        });
+    }
+
+
+    function mune() {
+        let html = "<div class='column' style='display: none;'>"+
+            "<ul>" +
+            "    <li>" +
+            "        <i class='iconfont icon-185078emailmailstreamline'></i>" +
+            "        <span>Contact Supplier</span>" +
+            "    </li>" +
+            "    <li>" +
+            "        <i class='iconfont icon-whatsapp'></i>" +
+            "        <span class='whatsapp'>whatsapp</span>" +
+            "    </li>" +
+            "    <li>" +
+            "        <i class='iconfont icon-wechat'></i>" +
+            "        <span class='wechat'>wechat</span>" +
+            "    </li>" +
+            "    <li class='ruiTouTop'>" +
+            "        <span class='top'>TOP</span>" +
+            "        <i class='iconfont icon-top'></i>" +
+            "    </li>" +
+            "</ul>" +
+            "</div>";
+        $('body').append(html);
+        // 按钮淡入淡出
+        $(window).scroll(function(){
+            if($(window).scrollTop() >= 450){
+                $(".column").fadeIn(200);
+            } else {
+                $(".column").stop(true,true).fadeOut(200);
+            }
+        });
+
+        // 回到顶部
+        $(".ruiTouTop").click(function(){
+            $("html,body").animate({
+                scrollTop:0
+            },200);
+        });
+    }
+
+    let j = {
+        swiperBanner: swiperBanner,
+        siblingsDom: siblingsDom,
+        mune: mune
+    };
+
+	// class Tools {
+     //
+     //    _$(id) {
+     //        return document.getElementById(id);
+     //    }
+    //
+	// 	swiperBanner() {
+	// 		var mySwiper = new Swiper ('.swiper-container', {
+	// 			loop: true,
+	// 			autoplay: true,
+	// 			pagination: {
+	// 				el: '.swiper-pagination',
+	// 				clickable: true,
+	// 			},
+	// 			navigation: {
+	// 				nextEl: '.swiper-button-next',
+	// 				prevEl: '.swiper-button-prev',
+	// 			},
+	// 		})
+	// 		$('.match_content_left_content').eq(0).show()
+	// 		$('.match_content_left_header_nav_item').click(function(){
+	// 			$('.match_content_left_header_nav_item').removeClass('active')
+	// 			$(this).addClass('active')
+	// 			var index = $('.match_content_left_header_nav_item').index($(this))
+	// 			$('.match_content_left_content').hide()
+	// 			$('.match_content_left_content').eq(index).fadeIn()
+	// 		})
+	// 	}
+    //
+	// 	siblings(dom,callback){
+     //        var pdom = dom.parentElement;
+     //        var tabArr = [].slice.call(pdom.children);
+     //        tabArr.filter(function(obj){
+     //            if(obj!=dom)callback.call(obj);
+     //        });
+     //    }
+    //
+     //    siblingsDom(id, class1, class2, flag) {
+     //        var cardDom = this._$(id);
+     //        var liDomes = cardDom.children;
+     //        var len = liDomes.length;
+     //        for(var i = 0; i < len; i++) {
+     //            var that = this;
+     //            //给对象缓存自有属性
+     //            liDomes[i].index = i;
+     //            liDomes[i].onmouseover = function() {
+     //                this.className = class1;
+     //                //同辈元素互斥
+     //                that.siblings(this,function(){
+     //                    this.className = class2;
+     //                });
+     //            }
+     //            liDomes[i].onmouseover = function(){
+     //                this.className = class1;
+     //                //同辈元素互斥
+     //                that.siblings(this, function(){
+     //                    this.className = class2;
+     //                });
+     //                //把对应的选项卡的内容显示出来
+     //                var tabDom = document.getElementById("IDate_list").children[this.index];
+     //                if (flag == true) {
+     //                    tabDom.style.display = "block";
+     //                    that.siblings(tabDom, function(){
+     //                        this.style.display = "none";
+     //                    });
+     //                }
+     //            };
+    //
+     //        }
+     //    }
+    //
+     //    getNewsMesasge() {
+     //    	$.ajax({
+     //    		url: './data/news.json',
+     //    		method: "GET",
+     //    		async: true,
+     //    		success: function(res) {
+     //    			console.log(res);
+     //    		},
+     //    		fail: function(err) {
+     //    			console.log(err);
+     //    		}
+     //    	});
+     //    }
+    //
+     //    mune() {
+     //        let html = "<div class='column' style='display: none;'>"+
+     //                    "<ul>" +
+     //                    "    <li>" +
+     //                    "        <i class='iconfont icon-185078emailmailstreamline'></i>" +
+     //                    "        <span>Contact Supplier</span>" +
+     //                    "    </li>" +
+     //                    "    <li>" +
+     //                    "        <i class='iconfont icon-whatsapp'></i>" +
+     //                    "        <span class='whatsapp'>whatsapp</span>" +
+     //                    "    </li>" +
+     //                    "    <li>" +
+     //                    "        <i class='iconfont icon-wechat'></i>" +
+     //                    "        <span class='wechat'>wechat</span>" +
+     //                    "    </li>" +
+     //                    "    <li class='ruiTouTop'>" +
+     //                    "        <span class='top'>TOP</span>" +
+     //                    "        <i class='iconfont icon-top'></i>" +
+     //                    "    </li>" +
+     //                    "</ul>" +
+     //                "</div>";
+     //        $('body').append(html);
+     //        // 按钮淡入淡出
+     //        $(window).scroll(function(){
+     //            if($(window).scrollTop() >= 450){
+     //                $(".column").fadeIn(200);
+     //            } else {
+     //                $(".column").stop(true,true).fadeOut(200);
+     //            }
+     //        });
+     //
+     //        // 回到顶部
+     //        $(".ruiTouTop").click(function(){
+     //            $("html,body").animate({
+     //                scrollTop:0
+     //            },200);
+     //        });
+     //    }
+    //
+	// }
 
     return {
-        Tools
+        j
     }
 
 })();
